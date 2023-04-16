45e132cf056be58c25b2d08aaf387d7baaa7fac0
Hyejin Kim <hjqueeen@gmail.com>
2023-04-16 13:55:35 +0200

    P23-0416 Add deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,6 @@
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './public/index.html',
-    './build/**/*.{js,jsx,ts,tsx}',
-  ],
-  darkMode: 'class',
+  content: ['./src/**/*.{ts,tsx,scss}'],
   theme: {
-    extend: {
-      maxHeight: {
-        'screen-2/3': '66.6666vh',
-      },
-      minHeight: {
-        'screen-1/3': '33.3333vh',
-        'screen-2/3': '66.6666vh',
-        'screen-5/6': '83.3333vh',
-      },
-      width: {
-        '1/5': '20%',
-        '2/5': '40%',
-        '3/5': '60%',
-        '4/5': '80%',
-      },
-    },
+    extend: {},
   },
 };
